f5c6972f209999662035d3742279fe39eb4ed10b
Lucas Possatti <lucas_possatti@hotmail.com>
2014-07-21 23:07:21 -0300

    Criação da query recursiva
    A query recursiva da atividade 6 foi feita.

---
+++ atividade-6/SQL Recursivo.sql
@@ -0,0 +1,21 @@
+﻿-- Exemplo de recursão do wikipedia
+/*
+WITH RECURSIVE tempf (n, fact) AS 
+(SELECT 0, 1 -- Initial Subquery
+  UNION ALL 
+ SELECT n+1, (n+1)*fact FROM tempf -- Recursive Subquery 
+        WHERE n < 9)
+SELECT * FROM tempf;
+*/
+
+-- CTE
+WITH RECURSIVE temp (n, ename, job, empno, mgr) AS
+(
+	SELECT 1, ename, job, empno, mgr
+	FROM emp WHERE mgr IS NULL
+		UNION ALL
+	SELECT t.n+1, e.ename, e.job, e.empno, e.mgr
+	FROM emp e, temp t WHERE e.mgr = t.empno
+)
+
+SELECT * FROM temp;
