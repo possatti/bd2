0236407e68178c6025ddb91506ed7bf31a54275e
Lucas Possatti <lucas_possatti@hotmail.com>
2014-07-24 19:08:39 -0300

    Inicio da segunda questão da atividade 4

--- atividade-4/catalogos-de-sistema.sql
+++ atividade-4/catalogos-de-sistema.sql
@@ -18,7 +18,7 @@ create table Trabalha (
     unique(id_empresa, id_pessoa));
 */
 
--- Pega o nome das colunas que são chaves primárias ou estrangeiras.
+-- 3) Pega o nome das colunas que são chaves primárias ou estrangeiras.
 SELECT relname "Nome da Tabela", attname "Nome da Coluna", contype "Tipo"
 FROM pg_constraint
 INNER JOIN pg_class ON conrelid = pg_class.oid
@@ -26,3 +26,27 @@ INNER JOIN pg_attribute ON attrelid = pg_class.oid
 WHERE
 	(contype = 'p' OR contype = 'f')
 	AND attnum = ANY (conkey);
+
+------------
+
+SELECT relname, attname, typname, attlen, attnotnull, atthasdef
+, adsrc
+FROM pg_class
+INNER JOIN pg_attribute ON attrelid = pg_class.oid
+INNER JOIN pg_type ON atttypid = pg_type.oid
+LEFT JOIN pg_attrdef ON pg_class.oid = adrelid AND adnum = attnum
+WHERE
+(
+	relname = 'pessoa'
+	OR relname = 'empresa'
+	OR relname = 'trabalha'
+)
+AND
+(
+	attname != 'cmax'
+	AND attname != 'cmin'
+	AND attname != 'xmin'
+	AND attname != 'xmax'
+	AND attname != 'ctid'
+	AND attname != 'tableoid'
+)
